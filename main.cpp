abae62dba90189acd5ca708571d7ac49d90b5b39
AlexJohnAnderson <alex.anderson095439@gmail.com>
2023-10-04 09:03:51 -0500

    added gradient effect to sphere

--- main.cpp
+++ main.cpp
@@ -26,12 +26,6 @@ bool hit_sphere(const point3& center, double radius, const ray& r) {
 
 //calculates the ray color as it interacts with the viewport
 color ray_color(const ray& r){
-    //check to see if the ray r hit the sphere.
-    //pass in the center point of the sphere, with a radius of 0.5, and the ray r
-    if (hit_sphere(point3(0,0,-1), 0.5, r)){
-        //if the check passes then return red
-        return color(1, 0, 0);
-    }
     //calculates the unit vector of the rays direction
     vec3 unit_direction = unit_vector(r.direction());
     //calculates a using the y component(verticl extension of the vector) of the normalized ray direction
@@ -39,6 +33,12 @@ color ray_color(const ray& r){
     //we multiply this by 0.5 to make the range [0, 1]
     //this remapping is done to create a gradient effect on the vertical axis
     auto a =0.5*(unit_direction.y() + 1.0);
+    //check to see if the ray r hit the sphere.
+    //pass in the center point of the sphere, with a radius of 0.5, and the ray r
+    if (hit_sphere(point3(0,0,-1), 0.5, r)){
+        //if the check passes then return red
+        return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(1.0, 0.7, 0.5);
+    }
     //returns a vec3 object with rgb set to 0
     //            white portion              blue poriton
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
