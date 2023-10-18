8213930e72e3762c3c0fc799988428dad71bd1fb
AlexJohnAnderson <alex.anderson095439@gmail.com>
2023-10-18 12:30:56 -0500

    updated common

--- common.h
+++ common.h
@@ -5,25 +5,22 @@
 #include <limits>
 #include <memory>
 
-
-// Usings
-
 using std::shared_ptr;
 using std::make_shared;
 using std::sqrt;
 
-// Constants
+//constants
 
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
-// Utility Functions
+//utility Functions
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
-// Common Headers
+//common Headers
 
 #include "ray.h"
 #include "vec3.h"
